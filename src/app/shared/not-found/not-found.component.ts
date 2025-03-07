76af89812aec8e3c590b2c337ab64e4b313a2879
harsh-kamde <harshkamde4321@gmail.com>
2025-03-07 12:01:00 +0530

    not found added

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
-
+  router = inject(Router);
+  navigateToHome(){
+    this.router.navigate(["/"]);
+  }
 }
